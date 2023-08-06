1ca29e738401c0bd2ff3106153ced117210ddbed
ShashikalaMokashi <116992756+ShashikalaMokashi@users.noreply.github.com>
2023-08-06 14:48:48 +0530

    Add files via upload

---
+++ prgm2.py
@@ -0,0 +1,8 @@
+#python prgm to find area of triangle
+base = float(input("Enter a base value::"))
+height = float(input("Enter height value::"))
+
+# base = 5
+# height = 8
+area = 0.5 * base * height
+print(f"area of triangle is {area}.") 
