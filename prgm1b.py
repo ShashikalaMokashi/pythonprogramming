7832cad11ab616fb2f47a1662107296522a57dd5
ShashikalaMokashi <116992756+ShashikalaMokashi@users.noreply.github.com>
2023-08-06 14:48:17 +0530

    Add files via upload

---
+++ prgm1b.py
@@ -0,0 +1,9 @@
+#write a python prgm to do arthimetic operation
+num1 = float(input("Enter 1st number::"))
+num2 = float(input("Enter second number::"))
+# num1 = 10
+# num2 = 20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
