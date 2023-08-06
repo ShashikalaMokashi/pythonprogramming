8c5734ed3a87d46d3fd1c00a75ccc78109144fc7
ShashikalaMokashi <116992756+ShashikalaMokashi@users.noreply.github.com>
2023-08-06 17:16:20 +0530

    Add files via upload

---
+++ prgm5.py
@@ -0,0 +1,4 @@
+# write a python program to convert celsius to farenhint
+celsius_temp = float(input("Enter a temperature value:"))
+fahrenheit_temp = (celsius_temp *9/5)+32
+print(f"{celsius_temp} degree celsius is equal to {fahrenheit_temp} degree fahrenheit. ")
